204978193b048ac7327481309b20e31de1827b7f
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2022-11-13 04:38:13 -0500

    Update webpage

--- arbora-frontend/src/pages/Plant.jsx
+++ arbora-frontend/src/pages/Plant.jsx
@@ -27,108 +27,116 @@ export default function Plant() {
       <form onSubmit={handleSubmit}>
         
           <div id = "type">
-              <label> Plant type:
+                <label> Plant type:
+                <input 
+                type="text"
+                name = "plantType"
+                value = {inputs.plantType || ""}
+                onChange = {handleChange}
+                />
+                </label>
+            </div>
+
+            <div id = "name">
+                <label> Growth Rate
+                <input 
+                type="text"
+                name = "growthRate"
+                value = {inputs.growthRate || ""}
+                onChange = {handleChange}
+                />
+                </label>
+            </div>
+
+            <div id = "max">
+
+                <label> Max Height
+                <input 
+                type="text"
+                name = "maxHeight"
+                value = {inputs.maxHeight|| ""}
+                onChange= {handleChange}
+                />
+                </label>
+            </div>
+        
+          
+      
+      <div id = "Percip">
+              <label> Ideal Percipitation
               <input 
               type="text"
-              name = "plantType"
-              value = {inputs.plantType || ""}
-              onChange = {handleChange}
+              name = "percip"
+              value = {inputs.percip|| ""}
+              onChange= {handleChange}
               />
               </label>
           </div>
 
-          <div id = "name">
-              <label> Growth Rate
+          <div id = "Temp">
+              <label> Ideal Temperature
               <input 
               type="text"
-              name = "growthRate"
-              value = {inputs.growthRate || ""}
-              onChange = {handleChange}
+              name = "temp"
+              value = {inputs.temp|| ""}
+              onChange= {handleChange}
               />
               </label>
           </div>
-
-          <div id = "max">
-
-              <label> Max Height
+          <div id = "lifeSpan">
+              <label> Life Span
               <input 
               type="text"
-              name = "maxHeight"
-              value = {inputs.maxHeight|| ""}
+              name = "lifeSpan"
+              value = {inputs.LifeSpan|| ""}
+              onChange= {handleChange}
+              />
+            </label>
+          </div>
+          
+      
+          
+          <div id = "Shade">
+              <label> Shade Tolerance
+              <input 
+              type="text"
+              name = "shadeTol"
+              value = {inputs.shadeTol|| ""}
               onChange= {handleChange}
               />
               </label>
           </div>
         
+          <div id = "seedsProduced">
+              <label> Seeds Produced
+              <input 
+              type="text"
+              name = "seedsProduced"
+              value = {inputs.seedProduced|| ""}
+              onChange= {handleChange}
+              />
+              </label>
+          </div>
+          <div id = "seedsProduction">
+              <label> Seeds Production
+              <input 
+              type="text"
+              name = "sedsProduction"
+              value = {inputs.seedProduction|| ""}
+              onChange= {handleChange}
+              />
+              </label>
+          </div>
+          
+     
+
+          
+
+          
+          <input type="submit" />
         
-
-        <div id = "Shade">
-            <label> Shade Tolerance
-            <input 
-            type="text"
-            name = "shadeTol"
-            value = {inputs.shadeTol|| ""}
-            onChange= {handleChange}
-            />
-            </label>
-        </div>
-
-        <div id = "Percip">
-            <label> Ideal Percipitation
-            <input 
-            type="text"
-            name = "percip"
-            value = {inputs.percip|| ""}
-            onChange= {handleChange}
-            />
-            </label>
-        </div>
-
-        <div id = "Temp">
-            <label> Ideal Temperature
-            <input 
-            type="text"
-            name = "temp"
-            value = {inputs.temp|| ""}
-            onChange= {handleChange}
-            />
-            </label>
-        </div>
-
-        <div id = "seedsProduced">
-            <label> Seeds Produced
-            <input 
-            type="text"
-            name = "seedsProduced"
-            value = {inputs.seedProduced|| ""}
-            onChange= {handleChange}
-            />
-            </label>
-        </div>
-
-        <div id = "seedsProduction">
-            <label> Seeds Production
-            <input 
-            type="text"
-            name = "sedsProduction"
-            value = {inputs.seedProduction|| ""}
-            onChange= {handleChange}
-            />
-            </label>
-        </div>
-
-        <div id = "lifeSpan">
-            <label> Life Span
-            <input 
-            type="text"
-            name = "lifeSpan"
-            value = {inputs.LifeSpan|| ""}
-            onChange= {handleChange}
-            />
-          </label>
-        </div>
-        <input type="submit" />
         
+         
 
 
       </form>
