5cdb863c9e0bc32f2db1dfa39cdce3b9a527dd83
Hamnah <hamnahq25@gmail.com>
2022-11-13 02:03:03 -0500

    added plant webpage with inputs

--- arbora-frontend/src/App.jsx
+++ arbora-frontend/src/App.jsx
@@ -4,20 +4,31 @@ import "./App.css";
 
 //Pages
 import Home from "./pages/Home";
+import Plant from "./pages/Plant";
+import Envir from "./pages/Envir";
 
 //Component
 import { Routes, Route } from "react-router-dom";
 import PageLink from "./components/nav/PageLink";
 import Navbar from "./components/nav/Navbar";
 
+//components
+
 function App() {
   return (
     <div id="App">
       <Navbar>
         <PageLink to="/">Home</PageLink>
+        <PageLink to = "/plant">Plant</PageLink>
+        <PageLink to = "/envir">Envirnoment</PageLink>
       </Navbar>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/plant" element={<Plant />}></Route>
+        <Route path="/envir" element={<Envir />}></Route>
+
+
+        
       </Routes>
     </div>
   );
---
+++ arbora-frontend/src/pages/Plant.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+import {useRef, useState} from "react";
+
+export default function Plant() {
+  
+
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  }
+
+
+  
+  return (
+    <main id="plant">
+      <h1>Plant</h1>
+      <h3>Input an plant into the simulation</h3>
+      <form onSubmit={handleSubmit}>
+        <div id = "type">
+            <label> Plant type:
+            <input 
+            type="text"
+            name = "plantType"
+            value = {inputs.plantType || ""}
+            onChange = {handleChange}
+            />
+            </label>
+        </div>
+
+        <div id = "name">
+            <label> Growth Rate
+            <input 
+            type="text"
+            name = "growthRate"
+            value = {inputs.growthRate || ""}
+            onChange = {handleChange}
+            />
+            </label>
+        </div>
+
+        <div id = "max">
+
+            <label> Max Height
+            <input 
+            type="text"
+            name = "maxHeight"
+            value = {inputs.maxHeight|| ""}
+            onChange= {handleChange}
+            />
+            </label>
+        </div>
+
+        <div id = "Shade">
+            <label> Shade Tolerance
+            <input 
+            type="text"
+            name = "shadeTol"
+            value = {inputs.shadeTol|| ""}
+            onChange= {handleChange}
+            />
+            </label>
+        </div>
+
+        <div id = "Percip">
+            <label> Ideal Percipitation
+            <input 
+            type="text"
+            name = "percip"
+            value = {inputs.percip|| ""}
+            onChange= {handleChange}
+            />
+            </label>
+        </div>
+
+        <div id = "Temp">
+            <label> Ideal Temperature
+            <input 
+            type="text"
+            name = "temp"
+            value = {inputs.temp|| ""}
+            onChange= {handleChange}
+            />
+            </label>
+        </div>
+
+        <div id = "seedsProduced">
+            <label> Seeds Produced
+            <input 
+            type="text"
+            name = "seedsProduced"
+            value = {inputs.seedProduced|| ""}
+            onChange= {handleChange}
+            />
+            </label>
+        </div>
+
+        <div id = "seedsProduction">
+            <label> Seeds Production
+            <input 
+            type="text"
+            name = "sedsProduction"
+            value = {inputs.seedProduction|| ""}
+            onChange= {handleChange}
+            />
+            </label>
+        </div>
+
+        <div id = "lifeSpan">
+            <label> Life Span
+            <input 
+            type="text"
+            name = "lifeSpan"
+            value = {inputs.LifeSpan|| ""}
+            onChange= {handleChange}
+            />
+          </label>
+        </div>
+        <input type="submit" />
+
+
+      </form>
+
+    </main>
+  );
+}
