7ade55120ec736557d674dc397b43dd7ab2c761e
linguini1 <matteo.golin@gmail.com>
2022-11-13 04:04:36 -0500

    API works

--- arbora-frontend/src/components/nav/Navbar.jsx
+++ arbora-frontend/src/components/nav/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Navbar({ children }) {
   return (
-    <nav class='my_nav' >
-      <h1 >Arbora</h1>
+    <nav className="my_nav">
+      <h1>Arbora</h1>
       <div id="nav-links">{children}</div>
     </nav>
   );
--- server.py
+++ server.py
@@ -4,35 +4,55 @@ __author__ = "Matteo Golin"
 # Imports
 from utils import load_plants_from_csv, load_environments_from_csv
 from classes.forest import Forest
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 # Constants
 def print_row(row):
     print([_.name[0] if _ else " " for _ in row])
 
 # Main
-app = Flask(__name__)
+app = Flask(__name__)  # Create server
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
-def main():
-    plant_dicts = load_plants_from_csv()
-    environments = load_environments_from_csv()
+
+# Starting data
+plant_dicts = load_plants_from_csv()
+environments = load_environments_from_csv()
+forest = Forest(
+    width=100,
+    height=100,
+    environment=environments["pukaskwa"],
+    plants=plant_dicts
+)
+
+
+# Endpoints
+@app.route("/increment", methods=["POST"])
+@cross_origin()
+def increment():
+    global forest
+    forest.increment(request.json["month"])
+    for row in forest.map:
+        print_row(row)
+    return forest.json()
+
+
+@app.route("/start", methods=["POST"])
+@cross_origin()
+def start():
+    environment = request.json.get("environment")
+    global forest
     forest = Forest(
-        width=20,
-        height=20,
-        environment=environments["pukaskwa"],
+        width=100,
+        height=100,
+        environment=environments[environment],
         plants=plant_dicts
     )
     forest.populate()
-    for row in forest.map:
-        print_row(row)
-    for _ in range(40):
-        for month in range(12):
-            forest.increment(month)
-
-            for row in forest.map:
-                print_row(row)
-            print()
+    return {'status': 200}
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8000, debug=True)
